656d9823285761f62cc023c8443003be83098585
Simon Oehrl <oehrl@vr.rwth-aachen.de>
2019-10-16 13:49:37 +0200

    Recognize headers without any extension

--- src/source-info.ts
+++ src/source-info.ts
@@ -1,15 +1,15 @@
-export const headerExtensions = ['hpp', 'h', 'hh'];
-export const sourceExtensions = ['cpp', 'c', 'cc', 'm', 'mm'];
+export const headerExtensions = ['.hpp', '.h', '.hh', ''];
+export const sourceExtensions = ['.cpp', '.c', '.cc', '.m', '.mm'];
 import * as path from "path";
 
 export function buildGlobForExtensions(extensions: string[], filenameBase?: string) {
-    return `**/${filenameBase || "*"}.{${extensions.join(",")}}`;
+    return `**/${filenameBase || "*"}{${extensions.join(",")}}`;
 }
 
 export function isHeaderFile(fileName: string) {
-    return headerExtensions.indexOf(path.extname(fileName).substr(1)) !== -1;
+    return headerExtensions.indexOf(path.extname(fileName)) !== -1;
 }
 
 export function isSourceFile(fileName: string) {
-    return sourceExtensions.indexOf(path.extname(fileName).substr(1)) !== -1;
+    return sourceExtensions.indexOf(path.extname(fileName)) !== -1;
 }
